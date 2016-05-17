8a59ff0c04881fa2694667bc6b935f58d915ec2d
excelsi0r <nuno_mgl@hotmail.com>
2016-05-17 18:37:04 +0100

    updated

--- generator.c
+++ generator.c
@@ -1,27 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 #include <pthread.h>
+
+#define FIFON "/tmp/fifoN"
+#define FIFOS "/tmp/fifoS"
+#define FIFOO "/tmp/fifoO"
+#define FIFOE "/tmp/fifoE"
+
+struct vehicle
+{
+  int id;
+  char port;
+};
+
+
+void * tvehicle(void * avg)
+{
+  pthread_detach(pthread_self());
+  int vid = *(int*) avg;
+  free(avg);
+  
+  /* gerar restantes caracteristicas, porta, tempo estacionamento*/
+  
+  /*criar fifo unico para leitura, abrir RW para não bloquear (nome do FIFO unico imporatante)*/
+  
+  //cuidado semaforo 
+  //sem_wait() iniciar a 1
+  
+  int fd = open(FIFON, O_WRONLY | O_NONBLOCK);
+  if(fd != -1)
+  {
+    write(fd, &vehicle, sizeof(vehicle));
+    close();
+    
+    read(fifounico, ...,...);
+    //entrar parque
+    read(....);
+  
+  }
+  //apagar fifo
+  //sem_post
+  return NULL;
+}
 
 int main(int argc, const char * argv[])
 {
+    //variables declaration
     long tps;
     double time_unit;
     pthread_t tid;
-    double time = 0.0;
-    double duration = (double) atoi(argv[1]);
+    double elapsed_time = 0.0;
+    int id = 0;
+    double duration;
     
-    
+    //test arguments
     if(argc != 3)
     {
       printf("Usage %s <GENERATOR_TIME> <TIME_UNIT_BETWEEN_ACTIONS>\n", argv[0]);
       return 1;
     }
     
+    //variables initialization
     tps = sysconf(_SC_CLK_TCK);
     time_unit = (double) atoi (argv[2])/tps;
-
-     
+    duration = (double) atoi(argv[1]);
     
+    //seed initialization
+    srand(time(NULL));
+    
+     while(elapsed_time <= duration)
+     {
+        int rn = rand() % 10;
+        if(rn < 3)
+        {
+          sleep(time_unit);
+          elapsed_time += time_unit;
+        }              
+        else if(rn < 5)
+        {
+            sleep(2*time_unit);
+            elapsed_time += 2*time_unit;
+        }
+        else{}
+        
+        int nr (int *) malloc(sizeof(in));
+        *nr = id++;
+        pthread_create(&tid, NULL, tvehicle, nr);
+        
+     }
     return 0;
 }
--- park.c
+++ park.c
@@ -1,8 +1,93 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+void * tusher(void * avg)
+{
+  //arrumar
+}
+
+void * tcontroller(void * avg)
+{
+  int close = 0;
+  
+  //criar extensao para N, S, O
+  int fd = open(FIFON, O_RDONLY);
+  
+  while(read(fd, &vehicle, sizeof(vehicle) != 0)
+  {
+     if(vehicle.id == -1)
+      closed = 1;
+     else if(closed)
+     {
+      //send back
+     }
+     else
+     {
+       //criar arrumador e passar vehicle
+     }
+  }
+  close(fd);
+  return NULL;
+
+}
+
+
 
 int main(int argc, const char * argv[])
 {
-    printf("Hello World\n");
+    //variables declaration
+    int dp, fplaces;
+    int duration;
+    pthread_t tidN, tidS, tidO, tidE;
+    int fdN, fdS, fdO, fdE;
+    
+
+    //test arguments
+    if(argc != 3)
+    {
+      printf("Usage %s <FREE_SPOTS_NUMBER> <TIME_UNTIL_CLOSE>\n", argv[0]);
+      return 1;
+    }
+    
+    //variable initialization
+    dp = fplaces = atoi(argv[1]);
+    duration = atoi(argv[2]);
+    
+    //creating fifos
+    mkfifo(FIFON, 0600);
+    pthread_create(&tidN, NULL, tcontroller, NULL);
+    fdN = open(FIFON, O_WRONLY);
+    
+    mkfifo(FIFOS, 0600);
+    pthread_create(&tidS, NULL, tcontroller, NULL);
+    fdN = open(FIFOS, O_WRONLY);
+    
+    mkfifo(FIFOE, 0600);
+    pthread_create(&tidE, NULL, tcontroller, NULL);
+    fdN = open(FIFOE, O_WRONLY);
+    
+    mkfifo(FIFOO, 0600);
+    pthread_create(&tidO, NULL, tcontroller, NULL);
+    fdN = open(FIFOO, O_WRONLY);
+    
+    sleep(duration);
+    
+    //falta criar vehicle struct com id = -1;
+    write(fdN, &vehicle_stop, sizeof(vehicle));
+    close(fdN);
+    
+    //espera outros threads
+    
+    unlink(FIFON);
+    unlink(FIFOS);
+    unlink(FIFOE);
+    unlink(FIFOO);
+    
     return 0;
 }
